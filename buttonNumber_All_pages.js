253947af3ed950be0500996d608db6323f0c12f3
mohsen <mohseen_abasi@yahoo.com>
2021-10-09 13:45:48 +0330

    change_js_name_btn_to_buttonNumber_all_pages_and_add into_amozeshpage_didgah

---
+++ buttonNumber_All_pages.js
@@ -0,0 +1,9 @@
+const pageItems = document.querySelectorAll('.page-item a');
+
+pageItems.forEach((item) => {
+	item.addEventListener('click', (e) => {
+		document.querySelector('.active').classList.remove('active');
+		e.preventDefault();
+		item.classList.add('active');
+	});
+});
